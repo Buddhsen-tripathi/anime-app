caaf5cc41d8247dfb532041d6065a172a6eedb54
Buddhsen-Tripathi <buddhsent19@gmail.com>
2022-04-10 17:43:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Nav from './Components/Nav';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+     <Nav/> 
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='F1'>
+      Footer
+    </div>
+  )
+}
+
+export default Footer
